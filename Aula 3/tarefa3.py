30680eb6442ed985a714c9bf4544b8a7f99f33c8
Arthur Briganti Gini <arthur.gini@brlink.com.br>
2020-10-09 11:56:50 -0300

    add tarefas

---
+++ Aula 3/tarefa3.py
@@ -0,0 +1,15 @@
+"""
+Autor: Arthur Briganti Gini
+RA: 213253
+e-mail: a213253@dac.unicamp.br
+
+
+Analisando os dados do jogo de dados CRAPS
+
+Você deverá simular diversas rodadas do CRAPS rastreando o número total de jogos ganhos e perdidos no primeiro lançamento, no segundo lançamento, no terceiro lançamento, etc. Resuma os resultados do seguinte modo:
+
+    Exibir um gráfico de barra horizontal indicando quantos jogos foram ganhos e como muitos são perdidos no primeiro lançamento, segundo lançamento, terceiro lançamento, etc. Uma vez que o jogo poderia continuar indefinidamente, você pode acompanhar as vitórias e derrotas na primeira dúzia lançamentos (de um par de dados), em seguida, manter dois contadores que controlam as vitórias e perdas após 12 lançamentos - não importa quanto tempo o jogo leve. Crie barras separadas para vitórias e derrotas.
+    Quais são as chances de ganhar no craps? [Nota: Você deve descobrir que craps é um dos jogos de casino mais justos. O que você acha que isso significa?]
+    Qual é a média para a duração de um jogo de dados? A mediana? O modo?
+    As chances de vitória aumentam com a duração do jogo?
+"""
